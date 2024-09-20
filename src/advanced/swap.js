b776ee9bb38e528c642e34e4e4e290ee674a927b
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-09-20 09:33:23 +0200

    finished core, extensions, advanced

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -2,5 +2,8 @@ let a = 8
 let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+const c = a
+a = b
+b = c
 
 module.exports = { a, b }
